26ac1342c9c869c34f0bc23bc8fcdb36019bdc21
xxxx <xxxx@gmail.com>
2022-02-08 20:24:19 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://i.pinimg.com/736x/7c/09/48/7c0948ac69a90aac021fe06632e5a667.jpg"
+          transition="scale-transition"
+          width="95"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn to="/"  text>
+        <span class="mr-2">Home</span>
+      </v-btn>
+       <v-btn to="/about"  text>
+        <span class="mr-2">about</span>
+      </v-btn>
+       <v-btn to="/Todo"  text>
+        <span class="mr-2">Todo</span>
+      </v-btn>
+    </v-app-bar>
+    
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/views/Todo.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <div>
+    
+    <MO @Input="validate"></MO>
+    <KO :K1="K1"></KO>
+    </div>
+    
+</template>
+
+<script>
+import MO from "../components/MO.vue"
+import KO from "../components/KO.vue"
+export default {
+  data: () => ({
+    K1: [],
+  }),
+components :{MO,KO},
+K1: "Input",
+  methods: {
+    validate(select, name) {
+      this.K1.push({
+        name: select + " : " + name,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
